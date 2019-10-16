0fdd6c73ef89d3c29de2d10baeebf982da26bc9a
Ram Eshwar Kaundinya <rkaundinya@g.ucla.edu>
2019-10-15 20:44:09 -0700

    Added in descriptive comments
    Comments now more thoroughly describe variabels used and the functions used.

--- LaserToBalloon.cpp
+++ LaserToBalloon.cpp
@@ -36,6 +36,8 @@ int main()
         std::cout << "I did not hit the balloon" << std::endl;
 }
 
+// Function to normalize a vector
+
 std::vector<float> normalize(std::vector<float> &toNormalize)
 {
     float magnitude; 
@@ -54,6 +56,11 @@ std::vector<float> normalize(std::vector<float> &toNormalize)
     return toNormalize;
 }
 
+// Function calculates the vector between a point of origin along the laser's path
+// and the center of the sphere. It returns this vector. 
+// @sphereCenter is the center of the sphere
+// @rayOrigin is the point along the laser's path being compared to
+
 std::vector<float> CalculateSphereToRayOriginVector(const std::vector<float> &sphereCenter, 
     const std::vector<float> &rayOrigin)
 {
@@ -64,6 +71,11 @@ std::vector<float> CalculateSphereToRayOriginVector(const std::vector<float> &sp
 
     return sphereToRayVec;     
 }
+
+// This function checks if the ray has somehow been fired from within a balloon
+// If this is true, then returns true
+// @sphereToRayVec is the vector between the sphere center and the ray point
+// @sphereRadius is the radius of the sphere
 
 bool CheckIfRayOriginatesWithinBalloon(const std::vector<float> &sphereToRayVec, 
     const float &sphereRadius)
@@ -77,12 +89,23 @@ bool CheckIfRayOriginatesWithinBalloon(const std::vector<float> &sphereToRayVec,
         return true;
 }
 
+// This function finds the point along the rays path closest to the sphere's center
+// @rayOriginToSphereOrigin is the vector between the sphere center and a point 
+// along the rays path
+// @rayDirection is the direction vector of the ray
+
 float RayPointClosestToSphereCenter(const std::vector<float> &rayOriginToSphereOrigin, 
     const std::vector<float> &rayDirection)
 {
         return std::inner_product(std::begin(rayOriginToSphereOrigin), 
             std::end(rayOriginToSphereOrigin), std::begin(rayDirection), 0.0f);
 }
+
+// This function checks if an intersection occurs between the ray and the sphere
+// If the rayPointClosest to sphere value is below 0, the ray is traveling 
+// away from the sphere and thus does not intersect
+// @rayPointClosestToSphereCenter is the length between a point along the ray path
+// and the point along the path closest to the sphere's center
 
 bool CheckIfIntersectionOccurs(const float &rayPointClosestToSphereCenter)
 {
@@ -91,6 +114,10 @@ bool CheckIfIntersectionOccurs(const float &rayPointClosestToSphereCenter)
     else
         return false;
 }
+
+// This function checks if the laser hits the balloon by pooling all 
+// functions outlined above in the correct algorithmic order
+// Returns true if the ray and sphere intersect
 
 bool CheckIfLaserHitsBalloon (const std::vector<float> &sphereCenter, 
     const float &sphereRadius, const std::vector<float> &rayOrigin, 
